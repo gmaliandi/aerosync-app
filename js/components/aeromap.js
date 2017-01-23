206264d9d94e383c62fd403895abba04968157c2
Guido Leandro Maliandi <guido.lm@gmail.com>
2017-01-22 23:11:09 -0300

    adding marker and plane icon for it

--- js/components/aeromap.js
+++ js/components/aeromap.js
@@ -13,18 +13,43 @@ const styles = StyleSheet.create({
 
 // TODO take location from user http://stackoverflow.com/questions/38122649/how-to-create-maps-which-detect-automatic-location-in-react-native
 class Aeromap extends Component {
+  constructor() {
+    super();
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      markers: [{
+        latlng: {latitude: 37.78825, longitude: -122.4324},
+        title: 'Nicanor',
+        description: 'Precaucion: suele despegar por la cabecera equivocada'
+      }]
+    };
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
   render() {
     return (
-     <View style ={styles.container}>
-       <MapView
-         style={styles.map}
-         region={{
-           latitude: 61.187189,
-           longitude: -149.829515,
-           latitudeDelta: 0.004757,
-           longitudeDelta: 0.006866,
-         }}
-       >
+      <View style ={styles.container}>
+        <MapView
+          style={styles.map}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}>
+            {this.state.markers.map(marker => (
+              <MapView.Marker
+                key={marker.latlng}
+                coordinate={marker.latlng}
+                title={marker.title}
+                description={marker.description}
+                image={require('../../assets/plane.png')}
+              />
+            ))}
        </MapView>
      </View>
     );
