a443a06dbe2bc260fafa373758e8010830339d7a
Guido Leandro Maliandi <guido.lm@gmail.com>
2017-01-22 23:23:02 -0300

    taking location from gps

--- js/components/aeromap.js
+++ js/components/aeromap.js
@@ -1,5 +1,7 @@
+/* global navigator */
+
 import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Dimensions, Alert} from 'react-native';
 import MapView from 'react-native-maps';
 
 const styles = StyleSheet.create({
@@ -11,16 +13,25 @@ const styles = StyleSheet.create({
   },
 });
 
-// TODO take location from user http://stackoverflow.com/questions/38122649/how-to-create-maps-which-detect-automatic-location-in-react-native
+let { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+
+// (Initial Static Location) Mumbai
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+
+const LATITUDE_DELTA = 0.01;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
 class Aeromap extends Component {
   constructor() {
     super();
     this.state = {
       region: {
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
       },
       markers: [{
         latlng: {latitude: 37.78825, longitude: -122.4324},
@@ -28,6 +39,38 @@ class Aeromap extends Component {
         description: 'Precaucion: suele despegar por la cabecera equivocada'
       }]
     };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA
+          }
+        });
+      },
+      (error) => Alert.alert('Error', error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      const newRegion = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      };
+
+      this.onRegionChange(newRegion);
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
   }
 
   onRegionChange(region) {
@@ -40,7 +83,7 @@ class Aeromap extends Component {
         <MapView
           style={styles.map}
           region={this.state.region}
-          onRegionChange={this.onRegionChange}>
+          onRegionChange={() => this.onRegionChange()}>
             {this.state.markers.map(marker => (
               <MapView.Marker
                 key={marker.latlng}
